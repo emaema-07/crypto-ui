924328649f772dfc5814df04bf9d7c608adc10e5
nirmalr16 <nirmalmcaraj@gmail.com>
2021-03-16 11:42:22 +0530

    tradehistory

--- src/components/dashboard/buycrypto.js
+++ src/components/dashboard/buycrypto.js
@@ -1,5 +1,5 @@
-import React, { useState,useEffect } from "react";
-import {  Button, Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 import { InlineIcon } from '@iconify/react';
 import btcIcon from '@iconify/icons-cryptocurrency/btc';
 import ethIcon from '@iconify/icons-cryptocurrency/eth';
@@ -18,156 +18,147 @@ import tusdIcon from '@iconify/icons-cryptocurrency/tusd';
 import usdcIcon from '@iconify/icons-cryptocurrency/usdc';
 import utkIcon from '@iconify/icons-cryptocurrency/utk';
 import { connect } from 'react-redux';
-import {actions} from '../../store/reducers/initial';
+import { actions } from '../../store/reducers/initial';
 
 const coinslist = [
-    { name: 'BTC - Bitcoin', icon: btcIcon, color: '#f69438' },
-    { name: 'DOGE - Dogecoin', icon: dogeIcon, color: '#c3a634' },
-    { name: 'ETH - Ethereum', icon: ethIcon, color: '#627eea' },
-    { name: 'USDT - Tether (ERC-20)', icon: usdtIcon, color: '#35a17b' },
-    { name: 'ADA - Cardano', icon: adaIcon, color: '#0d1e31' },
-    { name: 'BCH - Bitcoin Cash', icon: bchIcon, color: '#42c48b' },
-    { name: 'BAT - Basic Attention Token', icon: batIcon, color: '#fa533b' },
-    { name: 'LINK - Chainlink', icon: linkIcon, color: '#2e66da' },
-    { name: 'DAI - Dai', icon: daiIcon, color: '#fab238' },
-    { name: 'DASH - Dash', icon: dashIcon, color: '#028ce7' },
-    { name: 'LTC - Litecoin', icon: ltcIcon, color: '#a5a8a9' },
-    { name: 'OMG - OMG Network', icon: omgIcon, color: '#101010' },
-    { name: 'XRP - Ripple', icon: xrpIcon, color: '#222222' },
-    { name: 'TUSD - TrueUSD', icon: tusdIcon, color: '#7ac7bc' },
-    { name: 'USDC - USD Coin', icon: usdcIcon, color: '#2775ca' },
-    { name: 'UTK - Utrust', icon: utkIcon, color: '#31377a' },
-  
-  ]
+  { name: 'BTC - Bitcoin', icon: btcIcon, color: '#f69438' },
+  { name: 'DOGE - Dogecoin', icon: dogeIcon, color: '#c3a634' },
+  { name: 'ETH - Ethereum', icon: ethIcon, color: '#627eea' },
+  { name: 'USDT - Tether (ERC-20)', icon: usdtIcon, color: '#35a17b' },
+  { name: 'ADA - Cardano', icon: adaIcon, color: '#0d1e31' },
+  { name: 'BCH - Bitcoin Cash', icon: bchIcon, color: '#42c48b' },
+  { name: 'BAT - Basic Attention Token', icon: batIcon, color: '#fa533b' },
+  { name: 'LINK - Chainlink', icon: linkIcon, color: '#2e66da' },
+  { name: 'DAI - Dai', icon: daiIcon, color: '#fab238' },
+  { name: 'DASH - Dash', icon: dashIcon, color: '#028ce7' },
+  { name: 'LTC - Litecoin', icon: ltcIcon, color: '#a5a8a9' },
+  { name: 'OMG - OMG Network', icon: omgIcon, color: '#101010' },
+  { name: 'XRP - Ripple', icon: xrpIcon, color: '#222222' },
+  { name: 'TUSD - TrueUSD', icon: tusdIcon, color: '#7ac7bc' },
+  { name: 'USDC - USD Coin', icon: usdcIcon, color: '#2775ca' },
+  { name: 'UTK - Utrust', icon: utkIcon, color: '#31377a' },
+
+]
 
 const BuyCrypto = (props) => {
-    //   const history = useHistory();
-    const [currency, setCurrency] = useState("BTC - Bitcoin");
-    const [amount, setAmount] = useState(null);
-    const [count, setCount] = useState(null);
-    const [selcoin, setCoin] = useState("BTC");
+  //   const history = useHistory();
+  const [currency, setCurrency] = useState("BTC - Bitcoin");
+  const [amount, setAmount] = useState(null);
+  const [count, setCount] = useState(null);
+  const [selcoin, setCoin] = useState("BTC");
 
-    useEffect(() => {
-        props.getCurrencies();
-      }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    props.getCurrencies();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-      useEffect(() => {
-        if (props && props.currencylist) {
-          const data = props.currencylist;
-          setCoin(currency.split(" ")[0]);
-          const item = currency.split(" ", 1);
-          setAmount(data[item[0]].EUR);
-        }
-      }, [props]); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (props && props.currencylist) {
+      const data = props.currencylist;
+      setCoin(currency.split(" ")[0]);
+      const item = currency.split(" ", 1);
+      setAmount(data[item[0]].EUR);
+    }
+  }, [props]); // eslint-disable-line react-hooks/exhaustive-deps
 
-      const numberChange = event => {
-        setCount(event.target.value);
-      };
-      const onSubmit = () => {
-        const data = {
-          currency: currency,
-          amount: count / amount,
-          total: count,
-          singlecoin_amount: amount
-        };
-       
-        props.orderDetails(data);
-       
-      };
-    const handleChange = (event) => {
-        if(props && props.currencylist){
-          const data = props.currencylist;
-          setCoin(event.target.value.split(" ", 1))
-        const item = event.target.value.split(" ", 1);
-        setCurrency(event.target.value);
-        setAmount(data[item[0]].EUR);
-        }
-      };
-    return (
-        <div>   
-             <Form>
-                <p style={{ textAlign: 'left' }}>Currency</p>
-                    <Form.Control as="select" onChange={event => handleChange(event)}>
-                        {coinslist.map((item, index) => {
-                            return (
-                                <option value={item.name}>
-                            {/* <InlineIcon color={item.color} icon={item.icon} width={20} height={20} /> */}
-                            { item.name}
-                        </option>
-                            )
-                        })
-                    }
-                    </Form.Control>
-                    
-                <Form.Group >
-                    <p style={{ textAlign: 'left' }}>Amount</p>
-                    <Form.Control type="number" placeholder="Required" style={styles.textfield}  onChange={numberChange}/>
-                </Form.Group>
-            </Form>
+  const numberChange = event => {
+    setCount(event.target.value);
+  };
+  const onSubmit = () => {
+    const data = {
+      currency: currency,
+      amount: count / amount,
+      total: count,
+      singlecoin_amount: amount
+    };
 
-            <p >
-            Summary
+    props.orderDetails(data);
+
+  };
+  const handleChange = (event) => {
+    if (props && props.currencylist) {
+      const data = props.currencylist;
+      setCoin(event.target.value.split(" ", 1))
+      const item = event.target.value.split(" ", 1);
+      setCurrency(event.target.value);
+      setAmount(data[item[0]].EUR);
+    }
+  };
+  return (
+    <div>
+      <Form>
+        <p style={{ textAlign: 'left' }}>Currency</p>
+        <Form.Control as="select" onChange={event => handleChange(event)}>
+          {coinslist.map((item, index) => {
+            return (
+              <option value={item.name}>
+                {/* <InlineIcon color={item.color} icon={item.icon} width={20} height={20} /> */}
+                { item.name}
+              </option>
+            )
+          })
+          }
+        </Form.Control>
+        <Form.Group >
+          <p style={{ textAlign: 'left' }}>Amount</p>
+          <Form.Control type="number" placeholder="Required" style={styles.textfield} onChange={numberChange} />
+        </Form.Group>
+      </Form>
+      <p style={{ textAlign: "left" }}>
+        Summary
           </p>
-          <div >
-            <p >
-              {selcoin} Price
-            </p>
+      <div style={styles.head}>
+        <p >{selcoin} Price </p>
+        <p> €{amount ? parseFloat(amount).toFixed(2) : 0} </p>
+      </div>
 
-            <p>
-              €{amount ? parseFloat(amount).toFixed(2) : 0}
-            </p>
-
-            <p>You Get</p>
-
-            {count && (
-                  <p>
-                    ~{(count / amount).toFixed(6)}
-                    {selcoin}
-                  </p>
-                )}
-          </div>
-
-                <Button variant="warning" block  style={{marginTop:"8px"}} onClick={() => onSubmit()}>
-                    Buy Now
-                </Button>
+      {count && (
+        <div style={styles.head}>
+          <p>You Get</p>
+          <p>  ~{(count / amount).toFixed(6)} {selcoin} </p>
         </div>
-    );
+      )}
+
+      <Button variant="warning" block style={{ marginTop: "8px" }} onClick={() => onSubmit()}>
+        Buy Now
+      </Button>
+    </div>
+  );
 };
 
 
 const mapStateToProps = (state) => {
-    console.log(state.initial.currencyList,'state.initial.currencyList')
-    return {
-      currencylist: state.initial.currencyList
-    }
+  console.log(state.initial.currencyList, 'state.initial.currencyList')
+  return {
+    currencylist: state.initial.currencyList
   }
-  export default connect(
-    mapStateToProps,
-    {
-      orderDetails: actions.buyOrderDetails,
-      getCurrencies : actions.getCurrencies,
-    },
-  )(BuyCrypto);
+}
+export default connect(
+  mapStateToProps,
+  {
+    orderDetails: actions.buyOrderDetails,
+    getCurrencies: actions.getCurrencies,
+  },
+)(BuyCrypto);
 
 const styles = {
-    head: {
-        textAlign: "left",
-        display: "flex",
-        justifyContent: 'space-between'
-    },
-    head1: {
-        display: "flex",
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"row",
-    },
-    head2: {
-        display: "flex",
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"row",
-        border:"none",
-        width:"20%",
-        textAlign: "center",
-    },
+  head: {
+    textAlign: "left",
+    display: "flex",
+    justifyContent: 'space-between'
+  },
+  head1: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  head2: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    border: "none",
+    width: "20%",
+    textAlign: "center",
+  },
 };
--- src/components/dashboard/transactionHistory.js
+++ src/components/dashboard/transactionHistory.js
@@ -4,28 +4,32 @@ import { GrFormClose } from 'react-icons/gr';
 import { MdHourglassEmpty } from 'react-icons/md';
 
 const TransactionHistory = () => {
-    //   const history = useHistory();
-    const [qrCode, setQrCode] = useState(false);
-    return (
-        <div>
+  return (
+    <div>
+      <div style={styles.head}>
+        <Button style={{ borderRadius: '50px', backgroundColor: "#212529" }}>Verify</Button>
+        <Button style={{ borderRadius: '50px', backgroundColor: "#fd7e14" }}>AR</Button>
+      </div>
+      <div >
+        <Card style={{ width: '100%', height: "100%" }}>
+          <Card.Body >
             <div style={styles.head}>
-        <Button style={{ borderRadius:'50px', backgroundColor: "#212529" }}>Verify</Button>
-        <Button style={{ borderRadius:'50px', backgroundColor: "#fd7e14" }}>AR</Button>
-        </div>
-        <div >
-            <Card style={{ width: '100%', height: "100%"}}>
-                <Card.Body >
-                <div style={styles.head}>
-                    <h4 style={{textAlign: "left",}}>Transaction History</h4>
-                    <GrFormClose/>
-                    </div>
-                    
-                    <MdHourglassEmpty style={{height:"10%",width:"10%"}}/>
-                    <p>seems like your Transaction History box is empty</p>
-                </Card.Body>
-           </Card>
-        </div>
-        </div>
+              <h4 style={{ textAlign: "left", }}>Transaction History</h4>
+              <GrFormClose />
+            </div>
+            <div style={styles.head}>
+            <p> Bought BTC</p>
+            <p >15/09/2021</p>
+            <p >€2000</p>
+            </div>
+
+
+            {/* <MdHourglassEmpty style={{ height: "10%", width: "10%" }} />
+              <p>seems like your Transaction History box is empty</p> */}
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   );
 };
 
@@ -35,7 +39,7 @@ const styles = {
   head: {
     textAlign: "left",
     display: "flex",
-     justifyContent: 'space-between'
+    justifyContent: 'space-between'
   },
   head1: {
     padding: 5,
